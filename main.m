150d1a34c2038576b4918e2f62464cab2599e819
Mohamad.H Alizade <60823177+mohamad-h-alizade@users.noreply.github.com>
2022-03-30 12:12:50 +0430

    Add files via upload

---
+++ main.m
@@ -0,0 +1,30 @@
+clear 
+clc
+close all
+
+%% Change amp to achieve different inputs. amp = [0.1, 0.2, 0.3]
+amp = 0.3;
+
+%% defining parameters with sysms
+x = sym('x', [2,1]);
+x_dot = sym('x_dot', [2, 1]);
+syms u;
+
+x_equ = [0.2; 0];
+u_equ = 0.16;
+
+x_dot(1) = x(2);
+x_dot(2) = -x(1) - x(2) + u/(1-x(1));
+
+%% Taking jacobian and subbing it. 
+A = jacobian(x_dot, x);
+B = jacobian(x_dot, u);
+
+A = double(subs(A, [x; u], [x_equ; u_equ]))
+B = double(subs(B, [x; u], [x_equ; u_equ]))
+
+%% Computing the output
+mdl = 'non_linear_system.slx';
+open_system(mdl)
+sim(mdl);
+
